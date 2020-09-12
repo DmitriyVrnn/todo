0d451aa6e08fc34134442488a190d37a6e85a2fb
Dmitriy Voronin <dmitriy.vrnn@gmail.com>
2020-09-12 16:36:49 +0300

    Updated eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -9,14 +9,14 @@ module.exports = {
   },
   settings: {
     react: {
-      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: "detect",
     },
   },
   extends: [
-    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
   rules: {
     "react/prop-types": 0,
@@ -26,5 +26,6 @@ module.exports = {
     "react/jsx-props-no-spreading": 0,
     "import/no-unresolved": "off",
     "import/prefer-default-export": "off",
+    "max-len": ["error", { code: 120 }],
   },
 };
